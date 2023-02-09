96f63ec629a6b91f2c544f1477cd1fd7d49fa7bb
Alon Kigler <kiglerr@gmail.com>
2023-02-09 23:59:41 +0200

    template

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { lazy, Suspense, useEffect } from "react";
+import Index from "./jsx";
+
+import { connect, useDispatch } from "react-redux";
+import { Route, Switch, withRouter } from "react-router-dom";
+// action
+import { checkAutoLogin } from "./services/AuthService";
+import { isAuthenticated } from "./store/selectors/AuthSelectors";
+/// Style
+import "./vendor/bootstrap-select/dist/css/bootstrap-select.min.css";
+import "./css/style.css";
 
 function App() {
-  return <div className="App">lets go</div>;
+  return (
+    <Suspense
+      fallback={
+        <div id="preloader">
+          <div className="sk-three-bounce">
+            <div className="sk-child sk-bounce1"></div>
+            <div className="sk-child sk-bounce2"></div>
+            <div className="sk-child sk-bounce3"></div>
+          </div>
+        </div>
+      }
+    >
+      <Index />
+    </Suspense>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import ReactDOM from "react-dom";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import store from "./store";
 import { Provider } from "react-redux";
+import { store } from "./store/store";
+import reportWebVitals from "./reportWebVitals";
+import SimpleReactLightbox from "simple-react-lightbox";
+import ThemeContext from "./context/ThemeContext";
 
-window.store = store;
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </BrowserRouter>
-  </Provider>
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <SimpleReactLightbox>
+        <BrowserRouter basename="/react/demo">
+          <ThemeContext>
+            <App />
+          </ThemeContext>
+        </BrowserRouter>
+      </SimpleReactLightbox>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
+reportWebVitals();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
